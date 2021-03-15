c4120d6bf7e3fd3fea6cbdaf97f78731b720697b
Raul Sanchez <raulsg93@gmail.com>
2021-03-15 12:04:06 -0500

    added intro-sinatra

---
+++ intro-sinatra/app/controllers/application_controller.rb
@@ -0,0 +1,37 @@
+class ApplicationController < Sinatra::Base
+
+    set :views, "app/views" #we dont know this
+
+    get '/' do          #get request
+        "Hello World"
+    end
+
+    get '/time' do 
+        "Time.now"
+    end
+
+    get '/add/:num1/:num2' do 
+        # send_this = "Whats up, Lord Of the Strings!"
+        # send_this
+        answer  = params[:num1].to_i + params[:num2].to_i
+        "#{params[:num1]} + #{params[:num2]} = #{answer}"
+    end
+
+
+    get '/artists' do 
+        @artists = Artist.all
+        # artists
+        binding.pry
+        erb :index
+    end
+
+    get '/artists/:id' do
+        @artist = Artist.find(params[:id])
+        erb :show
+    end
+
+    
+
+    
+
+end
---
+++ intro-sinatra/db/seeds.rb
@@ -0,0 +1,11 @@
+Artist.destroy_all 
+
+
+Artist.create(name: "Angelo", age:28)
+Artist.create(name: "Hadi", age:25)
+Artist.create(name: "Emily", age:28)
+Artist.create(name: "Cori", age:28)
+Artist.create(name: "Devante", age:28)
+Artist.create(name: "Jose", age:28)
+Artist.create(name: "Ian", age:28)
+Artist.create(name: "Donovan", age:28)
